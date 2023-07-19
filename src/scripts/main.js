e7b29d38734abd267c1b35b3381e6380df06f852
Jane Shavrukova <eva.shavrukova@gmail.com>
2023-07-19 15:25:47 -0700

    Add Start Game

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write your code here
+const buttonStart = document.querySelector('.button.start');
+
+const messageStart = document.querySelector('.message-start');
+
+const cells = Array.from(document.querySelectorAll('.field-cell'));
+
+function startGame() {
+  buttonStart.textContent = 'Restart';
+  buttonStart.classList.remove('start');
+  buttonStart.classList.add('restart');
+
+  messageStart.classList.add('hidden');
+
+  clearField();
+  cellAppear();
+  cellAppear();
+}
+
+function clearField() {
+  cells.forEach(cell => {
+    cell.className = 'field-cell';
+    cell.textContent = '';
+  });
+}
+
+function cellAppear() {
+  const emptyCells = cells.filter(cell => cell.classList.length === 1);
+
+  const index = getRandomIntInclusive(0, emptyCells.length - 1);
+
+  emptyCells[index].classList.add('field-cell--2');
+  emptyCells[index].textContent = '2';
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+buttonStart.onclick = () => {
+  startGame();
+};
