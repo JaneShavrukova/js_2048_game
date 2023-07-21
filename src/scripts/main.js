02dc4ca2231725578552cb031feee4da364610a8
Jane Shavrukova <eva.shavrukova@gmail.com>
2023-07-21 16:26:40 -0700

    Add move right/left

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,9 @@ const messageStart = document.querySelector('.message-start');
 
 const cells = Array.from(document.querySelectorAll('.field-cell'));
 
+const cellsArr = [cells.slice(0, 4),
+cells.slice(4, 8), cells.slice(8, 12), cells.slice(12)];
+
 function startGame() {
   buttonStart.textContent = 'Restart';
   buttonStart.classList.remove('start');
@@ -30,17 +33,79 @@ function cellAppear() {
 
   const index = getRandomIntInclusive(0, emptyCells.length - 1);
 
-  emptyCells[index].classList.add('field-cell--2');
-  emptyCells[index].textContent = '2';
+  const cellValue = getNewValue();
+
+  emptyCells[index].classList.add(`field-cell--${cellValue}`);
+  emptyCells[index].textContent = cellValue;
 }
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+function getRandomIntInclusive(minimum, maximum) {
+  const min = Math.ceil(minimum);
+  const max = Math.floor(maximum);
 
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getNewValue() {
+  const random = Math.random();
+
+  if (random >= 0.9) {
+    return '4';
+  }
+
+  return '2';
+}
+
+function swap(arr, from, to) {
+  const tmpTextContent = arr[from].textContent;
+  const tmpClassName = arr[from].className;
+
+  arr[from].textContent = arr[to].textContent;
+  arr[from].className = arr[to].className;
+
+  arr[to].textContent = tmpTextContent;
+  arr[to].className = tmpClassName;
+}
+
+function move(rowCells) {
+  for (let i = 0; i < rowCells.length; i++) {
+    if (rowCells[i].textContent === '') {
+      for (let j = i; j < rowCells.length; j++) {
+        if (rowCells[j].textContent !== '') {
+          swap(rowCells, i, j);
+          break;
+        }
+      }
+    }
+  }
 }
 
 buttonStart.onclick = () => {
   startGame();
 };
+
+document.addEventListener('keydown', e => {
+  switch (e.key) {
+    case 'ArrowUp':
+      console.log('ArrowUp');
+      break;
+
+    case 'ArrowDown':
+      console.log('ArrowDown');
+      break;
+
+    case 'ArrowRight':
+      cellsArr.forEach(part => move([...part].reverse()));
+      cellAppear();
+
+      break;
+
+    case 'ArrowLeft':
+      cellsArr.forEach(part => move(part));
+      cellAppear();
+      break;
+
+    default:
+      break;
+  }
+});
